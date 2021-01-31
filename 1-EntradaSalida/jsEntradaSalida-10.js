855347f90b44e146fa0786f21eaa8f4e0d2eac40
Enzo.ezequiel <74376678+EnzoEzeZai@users.noreply.github.com>
2021-01-31 17:16:10 -0300

    Nuevos cambios

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	var sueldito;
+	sueldito=parseInt(txtIdImporte.value);
+
+	var descuen=sueldito*0.25;
+
+	var resul=sueldito-descuen;
+	
+	txtIdResultado.value=resul;
 }
