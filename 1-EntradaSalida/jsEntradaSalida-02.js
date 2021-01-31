420da76a44e3f0a527caaccfe3834d625e44e745
Enzo.ezequiel <74376678+EnzoEzeZai@users.noreply.github.com>
2021-01-31 16:27:05 -0300

    nuevos cambios

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,8 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("funciono perfectito");
+	var nombre=window.prompt("ingresa tu nombre");
+	var edad=prompt("ingresa tu edad");
+	alert("bienvenido "+nombre+" de "+edad+" años de edad");
 }
 
