3c304206494b7927a5f1455d4bc48350e9615ac9
Enzo.ezequiel <74376678+EnzoEzeZai@users.noreply.github.com>
2021-01-31 16:54:36 -0300

    Nuevos Cambios

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,46 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	
+	var primerNum=parseInt(txtIdNumeroUno.value);
+	
+	var segunNum=parseInt(txtIdNumeroDos.value);
+	
+	var suma=primerNum+segunNum;
+	alert("la suma es "+suma);
 }
 
 function restar()
 {
 	
+	var primerNum=parseInt(txtIdNumeroUno.value);
+	
+	var segunNum=parseInt(txtIdNumeroDos.value);
+	
+	var resta=primerNum-segunNum;
+
+	alert("la resta es "+resta);
 }
 
 function multiplicar()
 { 
+	var primerNum=parseInt(txtIdNumeroUno.value);
 	
+	var segunNum=parseInt(txtIdNumeroDos.value);
+	
+	var multi=primerNum*segunNum;
+
+	alert("la multiplicacion es "+multi);
 }
 
 function dividir()
 {
+	var primerNum=parseInt(txtIdNumeroUno.value);
 	
+	var segunNum=parseInt(txtIdNumeroDos.value);
+	
+	var divi=primerNum/segunNum;
+
+	alert("la division es "+divi);
 }
 
