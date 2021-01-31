a173bdc5fc2f1ef2c893eb8918aef203daae97eb
Enzo.ezequiel <74376678+EnzoEzeZai@users.noreply.github.com>
2021-01-31 16:48:07 -0300

    Nuevos Cambios

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,12 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var primerNum;
+	primerNum=parseInt(txtIdNumeroUno.value);
+	
+	var segunNum=parseInt(txtIdNumeroDos.value);
+	
+	var suma=primerNum+segunNum;
+	alert("la suma es "+suma);
 }
 
