5bbb3b725f1544af65b1928e806e2070e77fda39
Enzo.ezequiel <74376678+EnzoEzeZai@users.noreply.github.com>
2021-01-31 18:08:00 -0300

    nuevos cambios

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,18 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
 	
+	var edadIngresada=parseInt(txtIdEdad.value);
+	var estCivil=estadoCivil.value;
+	/*
+	if (estCivil!=Soltero && edadIngresada<18)
+	{
+		alert('Es muy pequeño para NO ser soltero.');
+	}*/
+	if(edadIngresada<18 && estCivil==Casado || estCivil==Divorciado)
+	{
+		alert('Es muy pequeño para NO ser soltero.');
+	}
+
 
 
 }//FIN DE LA FUNCIÓN
