4472d75157de552a1dfcaf14c630140094d2236e
Enzo.ezequiel <74376678+EnzoEzeZai@users.noreply.github.com>
2021-01-31 17:26:11 -0300

    nuevos cambios

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,15 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//tomo la edad 
+	var edadIngresada;
+	edadIngresada=parseInt(txtIdEdad.value);
+	if (edadIngresada==15){
+		alert("eres una niña bonita");
+	}
+	else
+	{
+		alert("tu no eres una niña bonita");
+	}
+	
 
 }//FIN DE LA FUNCIÓN
