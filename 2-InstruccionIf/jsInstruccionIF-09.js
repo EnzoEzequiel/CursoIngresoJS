97dea6916657d93a9497711704a66fdf205ed6da
Enzo.ezequiel <74376678+EnzoEzeZai@users.noreply.github.com>
2021-02-01 12:13:25 -0300

    nuevos cambios

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,7 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	//math round hace que no sea periodico, math.random es un metodo ()
+	var aleatorio = Math.round(Math.random()*10);
+	alert("Número aleatorio entre 1 y 10: "+aleatorio);
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,19 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+
+	var aleatorio = Math.round(Math.random()*10);
+	if(aleatorio==9 || aleatorio==10)
+	{
+		alert("EXCELENTE //Nota: "+aleatorio);
+	}
+	if(aleatorio>=4 && aleatorio<9)
+	{
+		alert("APROBÓ //Nota: "+aleatorio);
+	}
+	if(aleatorio<=4)
+	{
+		alert("Vamos, la proxima se puede //Nota: "+aleatorio);
+	}
+
 
 }//FIN DE LA FUNCIÓN
