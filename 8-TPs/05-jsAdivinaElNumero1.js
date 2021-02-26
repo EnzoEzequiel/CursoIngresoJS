e29d89ff2b1ac8f12fd5792248a7124d8ecb634a
Enzo.ezequiel <74376678+EnzoEzeZai@users.noreply.github.com>
2021-02-26 00:55:08 -0300

    cambios

--- 8-TPs/05-jsAdivinaElNumero1.js
+++ 8-TPs/05-jsAdivinaElNumero1.js
@@ -7,20 +7,45 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
-var contadorIntentos;
+let numeroSecreto; 
+let contadorIntentos;
+let numeroIngresado;
+contadorIntentos=0;
+numeroIngresado=parseInt(numeroIngresado);
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+  //"txtIdIntentos"
+  //"txtIdNumero"
 
+	//Genero el número RANDOM entre 1 y 100
+  numeroSecreto = Math.round(Math.random()*100);
+  console.log(numeroSecreto);
+  document.getElementById("txtIdIntentos").value=contadorIntentos;
+	//alert(numeroSecreto);
 }
 
 function verificar()
 {
-	
-	
+  numeroIngresado=document.getElementById("txtIdNumero").value;
+  if(numeroIngresado==numeroSecreto)
+  {
+    alert("Usted es un ganador!!!, con solo "+contadorIntentos+ " intentos");
+  }
+  else 
+  {
+    if(numeroIngresado<numeroSecreto)
+    {
+      alert("todavia falta para que llegues al numero secreto");
+      contadorIntentos++;
+    }
+    else if(numeroIngresado>numeroSecreto)
+    {
+      alert("te pasaste, continua con un numero mas pequeño");
+      contadorIntentos++;
+    }
+  }
+    
+
+
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,76 @@
 function mostrar()
 {
-	alert("tres");
+	//variables
+	let nombre;
+	let edad;
+	let sexo;
+	let estadoCivil;
+	let temperaturaCorporal;
+	let respuesta;
+	//acumuladores
+	let acumuladorTempMax;
+	let acumuladorNombreMax;
+	let acumEstadoCivilViudo;
+	let acumEstadoCivilSoltero;
+	let acumuladorViejoTemperatura;
+	let acumuladorEdad;
+	//promedio
+	let promedioEdad;
+	//flag
+	let BanderaPrimero;
+	//iniciar acumuladores
+	acumuladorTemperatura = 0;
+	acumuladorViejoTemperatura = 0;
+	acumuladorEdad = 0;
+	//iniciar flag
+	BanderaPrimero = 1;
+	//inicio del bucle
+	while(respuesta == "s")
+	{	//ingreso de datos
+		nombre = prompt("Ingrese su nombre");
+		edad = parseInt(prompt("Ingrese su edad"));
+		sexo = prompt("Ingrese su sexoo m o f");
+		estadoCivil = prompt("Ingrese su estado civil soltero, casado o viudo");
+		temperaturaCorporal = parseFloat(prompt("Ingrese su temperatura corporal"));
+		//fin del ingreso de datos
+		//4
+		if (BanderaPrimero == 1)
+		{
+			if (temperaturaCorporal > acumuladorTemperatura)
+			{
+				acumuladorTempMax = acumuladorTemperatura + temperaturaCorporal;
+				acumuladorNombreMax = nombre;
+				BanderaPrimero = 0;
+			}
+		}
+		//5
+		if( edad >= 18 && estadoCivil == "viudo")
+		{
+			acumEstadoCivilViudo ++;
+		}
+		//6
+		if (edad >= 18 && sexo == "m" && estadoCivil == "viudo" || estadoCivil == "soltero")
+		{
+			acumEstadoCivilSoltero ++;
+		}
+		//7
+		if (edad >= 60 && temperaturaCorporal >= 38)
+		{
+			acumuladorViejoTemperatura ++;
+		}
+		//8
+		if (sexo == "m" && estadoCivil == "soltero")
+		{	
+			acumuladorEdad = acumuladorEdad + edad;
+			promedioEdad = acumuladorEdad/acumEstadoCivilSoltero;
+		}
+
+		respuesta = prompt("Continuar s/n?")
+	} 
+	
+	console.log("La persona con mas tempratura es: "+ acumuladorNombreMax);
+	console.log("Hay "+ acumEstadoCivilViudo +" mayores de edad viudos");
+	console.log("La cantidad de hombres que hay solteros o viudos es: "+acumEstadoCivilSoltero);
+	console.log("Las personas de la tercera edad, tienen mas de 38 de temperatura son: " + acumuladorViejoTemperatura);
+	console.log("El promedio de edad entre los hombres solteros es: " + promedioEdad);
 }
